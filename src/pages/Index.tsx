ccdc9c1544fda5118294dbdc339d580ea8c927c3
Aniket Gavali <aniket.gavali@noovosoft.com>
2025-03-27 15:49:54 +0530

    Design bg

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -25,9 +25,9 @@ const Index = () => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 1 }}
-      className="min-h-screen bg-wedding-ivory relative"
+      className="min-h-screen relative"
     >
-
+<AnimatedBackground />
 
       <div className="max-w-5xl mx-auto px-4 py-8">
         <motion.div
